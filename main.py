d8304bde343b6c6a65cbad88c311c58db0756941
pranav1699 <pranavsadagopan@gmail.com>
2022-05-06 15:16:22 +0530

    added several changes

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ name = "Pranav"
 print("Hello {name}. How are you doing today?".format(name=name))
 print("This is from the dev branch")
 print("Added the new feature")
+print("Added the several new features")
