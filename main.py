4606ddeb6f2837e7d21ff1c76d17c902ffe4b5ec
pranav1699 <pranavsadagopan@gmail.com>
2022-05-11 13:05:39 +0530

    for loop added

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ print("Added the new feature")
 print("Added the several new features")
 print("Added the many new features")
 print("dev branch is ready to be merged")
+for i in range(1, 10):
+    print(i)
